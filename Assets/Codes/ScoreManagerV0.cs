09036de77ea863a01ea473966309c95cfd1598d0
Dmitry Kabak <userdima@gmail.com>
2025-10-05 16:00:35 +0200

    Add cursors

--- Assets/Codes/ScoreManagerV0.cs
+++ Assets/Codes/ScoreManagerV0.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using System;
+using UnityEngine.InputSystem;
 
 public class ScoreManagerV0 : MonoBehaviour
 {
@@ -10,10 +11,13 @@ public class ScoreManagerV0 : MonoBehaviour
     public float currentMoney = 100;
     public float maxNumberOfSpiders = 100;
     public float asmFeedPrice = 10;
+    public InputAction leftClick;
     public HideSpotV0[] allHidingSpots;
     public ProgressBarV0 fearProgressBar;
     public TextMeshPro moneyLabel;
     public TextMeshPro moneyDeltaLabel;
+    public Texture2D pressedCursor;
+    public Texture2D defaultCursor;
     public SpiderSpawnerV0 asm;
     private float lastMoney;
 
@@ -33,6 +37,21 @@ public class ScoreManagerV0 : MonoBehaviour
     {
         lastMoney = currentMoney;
         allHidingSpots = FindObjectsByType<HideSpotV0>(FindObjectsSortMode.None);
+    }
+
+
+    private void OnEnable()
+    {
+        leftClick.Enable();
+        leftClick.started += OnClickStart;
+        leftClick.canceled += OnClickEnd;
+    }
+
+    private void OnDisable()
+    {
+        leftClick.started -= OnClickStart;
+        leftClick.canceled -= OnClickEnd;
+        leftClick.Disable();
     }
 
     public void UpdateGUI()
@@ -73,5 +92,15 @@ public class ScoreManagerV0 : MonoBehaviour
         yield return new WaitForSeconds(0.9f);
         lastMoney = currentMoney;
         UpdateGUI();
+    }
+
+    void OnClickStart(InputAction.CallbackContext context)
+    {
+        Cursor.SetCursor(pressedCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnClickEnd(InputAction.CallbackContext context)
+    {
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
     }
 }
